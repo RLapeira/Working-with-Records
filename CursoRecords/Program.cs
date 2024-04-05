9a4e93cbaaf438645086c19b190420e019f5f961
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-05 12:31:37 +0200

    Classes and Reference Types Refresher

--- CursoRecords/Program.cs
+++ CursoRecords/Program.cs
@@ -4,9 +4,28 @@ namespace CursoRecords
 {
     class Program
     {
+        static void PrintCourse(Course course)
+        {
+            Console.WriteLine(course.Name);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var pluralsightCourse = new Course();
+            pluralsightCourse.Name = "Working with C# Records";
+            pluralsightCourse.Author = "roland Guijt";
+
+            PrintCourse(pluralsightCourse);
+
+            var anotherCourse = pluralsightCourse;
+
+            Console.WriteLine(pluralsightCourse.Name);
+            Console.WriteLine(anotherCourse.Name);
         }
+    }
+
+    public class Course
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
     }
 }
