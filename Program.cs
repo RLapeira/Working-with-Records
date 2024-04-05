35204605c4c85ab438d01d4c24b4f52f0b7c4353
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-05 19:35:52 +0200

    Converting to a String

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text.StringBuilder;
+
+namespace ClassesAndRecords
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CourseRecord classroomCourse = new TimedCourseRecord(
+                "Working with C# Records",
+                "Roland Guijt",
+                Duration: 90
+            );
+
+            Console.WriteLine(classroomCourse);
+        }
+
+        public record CourseRecord(string Name, string Author)
+        {
+
+        }
+
+        public record TimedCourseRecord(string Name, string Author, int Duration)
+            : CourseRecord(Name, Author)
+        {
+            public string Venue { get; init; }
+
+            //public override string ToString()
+            //{
+            //    return $"{Name} - {Author}";
+            //} -> o este método, o el PrintMembers, pero no ambos
+
+            protected override bool PrintMembers(StringBuilder builder)
+            {
+                if (base.PrintMembers(builder))
+                {
+                    builder.Append(", ");
+                }
+                builder.Append("Duration");
+                builder.Append(" = ");
+                builder.Append(this.Duration.ToString());
+                builder.Append(", ");
+                builder.Append("Venue");
+                builder.Append(" = ");
+                builder.Append((object)this.Venue);
+                builder.Append("Title");
+                builder.Append(" = ");
+                builder.Append($"{Name} - {Author}");
+                return true;
+            }
+        }
+
+    }
+}
